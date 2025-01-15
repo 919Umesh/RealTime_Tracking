87f5fa0408e44ec80a4f82685eb76c20d98153b0
Umesh <globaltechumesh11@gmail.com>
2025-01-15 13:06:13 +0545

    Firebase Added

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,31 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:live_track/models/product_model.dart';
+
+
+class Cart extends Equatable {
+  final List<Product> products;
+
+  const Cart({this.products = const <Product>[]});
+
+  Map productQuantity(products) {
+    var quantity = Map();
+
+    products.forEach((product) {
+      if (!quantity.containsKey(product)) {
+        quantity[product] = 1;
+      } else {
+        quantity[product] += 1;
+      }
+    });
+    return quantity;
+  }
+
+  double get subtotal => products.fold(
+      0, (total, current) => total + double.parse(current.currentPrice));
+
+  String get totalString => subtotal.toStringAsFixed(2);
+
+  @override
+  List<Object?> get props => [products];
+}
